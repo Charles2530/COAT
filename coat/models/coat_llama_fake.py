c8b305638b9ca33e5f0caef96d8c14948e15f0bf
Charles2530 <2569337619@qq.com>
2025-12-25 18:13:06 +0800

    Debug: fix coat_llama_fake.py

--- coat/models/coat_llama_fake.py
+++ coat/models/coat_llama_fake.py
@@ -135,13 +135,13 @@ class LlamaDecoderLayerFake(nn.Module):
         hidden_states: torch.Tensor,
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
-        past_key_values: Optional[Cache] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: bool = False,
         use_cache: Optional[bool] = False,
         cache_position: Optional[torch.LongTensor] = None,
         position_embeddings: Optional[tuple[torch.Tensor, torch.Tensor]] = None,
         **kwargs,
     ) -> Any:
-        output_attentions = kwargs.get("output_attentions", False)
 
         residual = hidden_states
         hidden_states = self.input_layernorm(hidden_states)
@@ -150,7 +150,8 @@ class LlamaDecoderLayerFake(nn.Module):
             hidden_states=hidden_states,
             attention_mask=attention_mask,
             position_ids=position_ids,
-            past_key_values=past_key_values,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
             use_cache=use_cache,
             cache_position=cache_position,
             position_embeddings=position_embeddings,

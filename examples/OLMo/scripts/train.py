6ba1d9cbf08eeb7b1563392ed04f265aeadfd338
Charles2530 <2569337619@qq.com>
2026-04-10 09:49:20 +0800

    OLMo: update fake_quant configs, reproduce YAMLs, and train scripts

--- examples/OLMo/scripts/train.py
+++ examples/OLMo/scripts/train.py
@@ -157,6 +157,7 @@ def main(cfg: TrainConfig) -> None:
         olmo_model = OLMo(cfg.model)
     else:
         olmo_model = OLMo(cfg.model)
+    log.info(f"model type:{type(olmo_model)} with {cfg.quantize_model.use_quantize_model}")
     log.info(f"Total number of parameters: {olmo_model.num_params():,d}")
     log.info(f"Number of non-embedding parameters: {olmo_model.num_params(include_embedding=False):,d}")
     log.info(f"Peak GPU Memory (MB) before {cfg.distributed_strategy}: {int(peak_gpu_memory() or 0)}")
